4de5b330dbabe5071e4bb0eb654dfc3fb410fadc
Beloha18 <Scottallemand7@gmail.com>
2023-02-01 22:07:38 +0100

    Andrana

---
+++ src/test/java/com/example/ezaka/EzakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
